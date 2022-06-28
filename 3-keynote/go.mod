ad52cec3f0c85f321b9733cdb5ce572279aca377
marcy-t <marcy.workout.life@gmail.com>
2022-06-29 03:08:26 +0900

    Wip

--- 3-keynote/go.mod
+++ 3-keynote/go.mod
@@ -2,7 +2,10 @@ module github.com/sample-go-ethereum
 
 go 1.17
 
-require github.com/ethereum/go-ethereum v1.10.19
+require (
+	github.com/ethereum/go-ethereum v1.10.19
+	github.com/k0kubun/pp/v3 v3.1.0
+)
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
@@ -10,7 +13,10 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- 3-keynote/main.go
+++ 3-keynote/main.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/k0kubun/pp/v3"
 )
 
-func createKs() {
+func createKs() accounts.Account {
 	/*
 		・KeyStoreは、暗号化されたウォレットプライベートキーを含むファイル
 		・「go-ethereum」のキーストアには、1ファイルにつき1つのウォレットキーペアしか格納できない
@@ -17,21 +19,28 @@ func createKs() {
 		・その後、NewAccountを呼び出し暗号化用のパスワードを渡すことで新しいウォレットを生成できます
 		・NewAccountを呼び出すたびに、新しいキーストアファイルがディスク上に生成されます
 	*/
-	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+	ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
+
+	log.Println("########")
+	log.Println("ks : ", ks)
+	log.Println("########")
+
 	password := "secret"
 	account, err := ks.NewAccount(password)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%v \n", "------------------------------------------")
-	fmt.Printf("account : %v \n", account)
+	fmt.Printf("account Address: %v \n", account.Address.Hex())
+	fmt.Printf("account : %v \n", account.URL)
 	/*
 		account : {0xdb510F95Dc75b292E7E3F12B57dF3840f6e6A358 keystore:///Users/makito/B-LAND/sample-go-ethereum/3-keynote/tmp/UTC--2022-06-28T12-06-12.353488000Z--db510f95dc75b292e7e3f12b57df3840f6e6a358}
 	*/
+	return account
 }
 
 func importKs() {
-	file := "./tmp/UTC--2022-06-28T12-44-14.249656000Z--bee2c6283c97a486c899562947da180d053e4086"
+	file := "./wallets/UTC--2022-06-28T18-07-03.968841000Z--497ce35065662c265311e48248af55b939e069cb"
 
 	fmt.Printf("%v \n", "------------------------------------------")
 	fmt.Printf("keystore.StandardScryptN : %v \n", keystore.StandardScryptN)
@@ -40,11 +49,9 @@ func importKs() {
 	fmt.Printf("keystore.StandardScryptP : %v \n", keystore.StandardScryptP)
 
 	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+
 	jsonBytes, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Println("####")
-		log.Println("err : ", err)
-		log.Println("####")
 		log.Fatal(err)
 	}
 
@@ -52,18 +59,52 @@ func importKs() {
 		・鍵をインポートするには、基本的に通常通りNewKeyStoreを再起動し、
 		鍵ストアのJsonデータをバイトとして受け取るImportメソッドを呼び出す必要がある
 	*/
-	password := "secret"
+	passPhrase := "secret"
+	// newPassphrase := "hoge"
 	/*
 		・第二引数は暗号化したパスワードで、これを複合化するために使用
 		・第三者引数は新しい暗号化用パスワードを指定するが、例では同じものを使用
 		・以下はキーストアをインポートしてアカウントにアクセス
 		・鍵ストアをインポートしてアカウントにアクセスする例
 	*/
-	account, err := ks.Import(jsonBytes, password, password)
+	/*
+		https://pkg.go.dev/github.com/0xsequence/ethkit/go-ethereum/accounts/keystore#KeyStore.Import
+		func (ks *KeyStore) Import(keyJSON []byte, passphrase, newPassphrase string) (accounts.Account, error)
+	*/
+	account, err := ks.Import(jsonBytes, passPhrase, passPhrase)
 	if err != nil {
 		// could not decrypt key with given password
+		// account already exists
 		log.Fatal(err)
 	}
+
+	fmt.Printf("%v \n", "------------------------------------------")
+	fmt.Printf("account : %v \n", account.Address.Hex())
+
+	// if err := os.Remove(file); err != nil {
+	// 	log.Fatal(err)
+	// }
+}
+
+func updateKs() {
+	file := "./wallets/UTC--2022-06-28T16-43-32.884125000Z--77e080a900be5f0f5b9f76c87e80394382117f20"
+
+	ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
+	jsonBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account, err := ks.Import(jsonBytes, "secret", "secret")
+	if err != nil {
+		// could not decrypt key with given password
+		// account already exists
+		pp.Print(err)
+		log.Fatal(err)
+	}
+
+	passPhrase := "secret"
+	_ = ks.Update(account, passPhrase, passPhrase)
 
 	fmt.Printf("%v \n", "------------------------------------------")
 	fmt.Printf("importKs Address : %v \n", account.Address.Hex())
@@ -77,6 +118,9 @@ func main() {
 	/*
 
 	 */
-	// createKs()
+	// account := createKs()
+	// fmt.Printf("%v \n", "------------------------------------------")
+	// fmt.Printf("account createKs() :  %v \n", account)
 	importKs()
+	// updateKs()
 }
